d3fe3ec38b002dd50f03bf32bcf56c4bbfeedfef
Skr <oscaraparisisancho@gmail.com>
2019-02-11 12:40:31 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-maskotin/precache-manifest.2be6d7fdd207c0b863904fe1bf633a93.js"
+  "/React-Maskotin/precache-manifest.c5319a35b5bcce3893400d85e13fc286.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-maskotin/index.html", {
+workbox.routing.registerNavigationRoute("/React-Maskotin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
